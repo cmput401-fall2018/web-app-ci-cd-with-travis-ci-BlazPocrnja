fac0e6c5f8ed3687e5c3a8b8a55df945b3bcdbfe
BlazPocrnja <bpocrnja@gmail.com>
2018-11-02 22:37:48 -0600

    Added chromium to travis.yml

--- selenium_test.py
+++ selenium_test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 
 def test_home():
     driver = webdriver.Chrome()
-    driver.get("http://162.246.157.124:8000/")
+    driver.get("http://127.0.0.1:8000")
 
     time.sleep(5)
 
