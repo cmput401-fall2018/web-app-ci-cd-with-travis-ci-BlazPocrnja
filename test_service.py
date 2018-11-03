e469752c7e5e36876653711d555cbfd3c328848d
BlazPocrnja <bpocrnja@gmail.com>
2018-11-02 22:08:58 -0600

    Added test_service

---
+++ test_service.py
@@ -0,0 +1,27 @@
+from service import Service
+from unittest.mock import patch
+
+service = Service()
+
+@patch('service.Service.bad_random', return_value=10)
+def test_bad_random(bad_random):
+ 	assert service.bad_random() == 10
+
+@patch('service.Service.bad_random', return_value=10)
+def test_divide(bad_random):
+	assert service.divide(10) == 1
+ 	assert service.divide(1) == 10
+	assert service.divide(-1) == -10
+	assert service.divide(2) == 5
+
+ def test_abs_plus():
+ 	assert service.abs_plus(-1) == 2
+	assert service.abs_plus(1) == 2
+	assert service.abs_plus(0) == 1
+	assert service.abs_plus(-0.5) == 1.5
+
+@patch('service.Service.bad_random', return_value=10)
+def test_complicated_function(bad_random):
+ 	assert service.complicated_function(10) == (1, 0)
+	assert service.complicated_function(-2) == (-5, 0)
+	assert service.complicated_function(0.5) == (2, 0)
